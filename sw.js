106dd2a71e40113142d11b8a91006976d0a893ea
fidelejunior <fidelejunior12@gmail.com>
2026-04-16 07:34:45 +0100

    Refactor service worker install and fetch events
    Removed caching logic and added console log on install.

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-const CACHE = 'montechnicien-v1';
-const FILES = ['/'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installé");
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
